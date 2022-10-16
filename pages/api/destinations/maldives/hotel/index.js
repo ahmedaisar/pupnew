e5f1d891f389d0565da81ebb2d5ac06b10a3ef4a
Ahmed <ahmed.aisar@gmail.com>
2022-10-16 11:35:01 +0500

    api#working-3

--- pages/api/destinations/maldives/hotel/index.js
+++ pages/api/destinations/maldives/hotel/index.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     const page = await browser.newPage();
 
     await page.goto(
-      `https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${rooms}&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=hotel,bed_and_breakfast,guest_house,resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
+      `https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${rooms}&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
       {
         waitUntil: "networkidle2",
       }
