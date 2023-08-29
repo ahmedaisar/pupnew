5b3a17a669171cf1fd60e155c386b263f523927b
0x-m1cro <0xm1cro@proton.me>
2023-08-29 14:20:43 +0500

    updated apis

--- pages/api/destinations/maldives/hotel/index.js
+++ pages/api/destinations/maldives/hotel/index.js
@@ -1,34 +1,34 @@
-const chrome = require("chrome-aws-lambda");
+// const chrome = require("chrome-aws-lambda");
 
-export default async function handler(req, res) {
-  let query = req.query;
-  const { hotelid, checkin, checkout, rooms } = query;
+// export default async function handler(req, res) {
+//   let query = req.query;
+//   const { hotelid, checkin, checkout, rooms } = query;
 
-  const options = {
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless,
-  };
+//   const options = {
+//     args: chrome.args,
+//     executablePath: await chrome.executablePath,
+//     headless: chrome.headless,
+//   };
 
-  try {
-    const browser = await chrome.puppeteer.launch(options);
+//   try {
+//     const browser = await chrome.puppeteer.launch(options);
 
-    const page = await browser.newPage();
+//     const page = await browser.newPage();
 
-    await page.goto(
-      `https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${rooms}&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
-      {
-        waitUntil: "networkidle2",
-      }
-    );
-    //await page.waitForTimeout(2000);
-    let html = await page.evaluate(() => {
-      return JSON.parse(document.querySelector("body").innerText);
-    });
-    await browser.close();
-    res.status(200).json(html);
-    console.log(html);
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     await page.goto(
+//       `https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${rooms}&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
+//       {
+//         waitUntil: "networkidle2",
+//       }
+//     );
+//     //await page.waitForTimeout(2000);
+//     let html = await page.evaluate(() => {
+//       return JSON.parse(document.querySelector("body").innerText);
+//     });
+//     await browser.close();
+//     res.status(200).json(html);
+//     console.log(html);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
--- pages/api/expedia/index.js
+++ pages/api/expedia/index.js
@@ -1,66 +1,66 @@
-const chrome = require("chrome-aws-lambda");
+// const chrome = require("chrome-aws-lambda");
 
-export default async function handler(req, res) {
-  //   let query = req.query;
-  //   const { hotelid, checkin, checkout, rooms } = query;
-  //   const args = [
-  //     "--no-sandbox",
-  //     "--disable-setuid-sandbox",
-  //     "--disable-infobars",
-  //     "--window-position=0,0",
-  //     "--ignore-certifcate-errors",
-  //     "--ignore-certifcate-errors-spki-list",
-  //     '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
-  //   ];
-  const options = {
-    args: await chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: await chrome.headless,
-  };
+// export default async function handler(req, res) {
+//   //   let query = req.query;
+//   //   const { hotelid, checkin, checkout, rooms } = query;
+//   //   const args = [
+//   //     "--no-sandbox",
+//   //     "--disable-setuid-sandbox",
+//   //     "--disable-infobars",
+//   //     "--window-position=0,0",
+//   //     "--ignore-certifcate-errors",
+//   //     "--ignore-certifcate-errors-spki-list",
+//   //     '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
+//   //   ];
+//   const options = {
+//     args: await chrome.args,
+//     executablePath: await chrome.executablePath,
+//     headless: await chrome.headless,
+//   };
 
-  try {
-    const browser = await chrome.puppeteer.launch(options);
+//   try {
+//     const browser = await chrome.puppeteer.launch(options);
 
-    const page = await browser.newPage();
-    await page.setViewport({ width: 0, height: 0 });
-    await page.goto(
-      `https://www.expedia.com/Hotel-Search?adults=2&d1=2023-02-01&d2=2023-02-06&destination=Maldives&endDate=2023-02-06&latLong=3.202778%2C73.22068&regionId=109&rooms=1&semdtl=&sort=RECOMMENDED&startDate=2023-02-01&theme=&useRewards=false&userIntent=`,
-      {
-        waitUntil: "networkidle2",
-      }
-    );
-    // await page.waitForTimeout(3000);
-    let xhrCatcher = await page.waitForResponse((r) =>
-      r.request().url().includes("https://www.expedia.com/graphql")
-    );
+//     const page = await browser.newPage();
+//     await page.setViewport({ width: 0, height: 0 });
+//     await page.goto(
+//       `https://www.expedia.com/Hotel-Search?adults=2&d1=2023-02-01&d2=2023-02-06&destination=Maldives&endDate=2023-02-06&latLong=3.202778%2C73.22068&regionId=109&rooms=1&semdtl=&sort=RECOMMENDED&startDate=2023-02-01&theme=&useRewards=false&userIntent=`,
+//       {
+//         waitUntil: "networkidle2",
+//       }
+//     );
+//     // await page.waitForTimeout(3000);
+//     let xhrCatcher = await page.waitForResponse((r) =>
+//       r.request().url().includes("https://www.expedia.com/graphql")
+//     );
 
-    let xhrResponse = await xhrCatcher;
+//     let xhrResponse = await xhrCatcher;
 
-    let xhrPayload = await xhrResponse.json();
-    console.log("xhrPayload", xhrPayload);
+//     let xhrPayload = await xhrResponse.json();
+//     console.log("xhrPayload", xhrPayload);
 
-    await browser.close();
-    res.status(200).json(xhrPayload);
-  } catch (error) {
-    console.log(error);
-  }
-  // await page.type('#input-auto-complete > input[type="text"]', "Maldives");
-  // await page.click(
-  //   '.SearchFormFlyout_formWrapper__CLXm > input[type="submit"]'
-  // );
-  // await page.waitForTimeout(5000);
-  // const [response] = await Promise.all([
-  //   page.waitForResponse((res) =>
-  //     res.url().includes("/graphql?infoSlideoutQuery")
-  //   ),
-  // ]);
-  // const jsondata = await response.json();
-  // let jsn;
+//     await browser.close();
+//     res.status(200).json(xhrPayload);
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   // await page.type('#input-auto-complete > input[type="text"]', "Maldives");
+//   // await page.click(
+//   //   '.SearchFormFlyout_formWrapper__CLXm > input[type="submit"]'
+//   // );
+//   // await page.waitForTimeout(5000);
+//   // const [response] = await Promise.all([
+//   //   page.waitForResponse((res) =>
+//   //     res.url().includes("/graphql?infoSlideoutQuery")
+//   //   ),
+//   // ]);
+//   // const jsondata = await response.json();
+//   // let jsn;
 
-  // await page.on("response", async (res) => {
-  //   if (res) {
-  //     page.waitForResponse((res) => res.url().includes("/graphql"));
-  //   }
-  //   console.log(await res.json());
-  // });
-}
+//   // await page.on("response", async (res) => {
+//   //   if (res) {
+//   //     page.waitForResponse((res) => res.url().includes("/graphql"));
+//   //   }
+//   //   console.log(await res.json());
+//   // });
+// }
