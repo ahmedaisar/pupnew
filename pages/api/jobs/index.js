9617c9133f534c8f446d688f5744eb3480f177ee
0x-m1cro <0xm1cro@proton.me>
2023-08-29 14:19:25 +0500

    updated jobs api

--- pages/api/jobs/index.js
+++ pages/api/jobs/index.js
@@ -1,4 +1,4 @@
-const chrome = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer");
 const cheerio = require('cheerio');
 const fs = require('fs');
 
@@ -7,12 +7,12 @@ export default async function handler(req, res) {
 
   const options = {
     //args: chrome.args,
-    executablePath: await chrome.executablePath,
+    executablePath: await puppeteer.executablePath,
    // headless: chrome.headless,
   };
 
   try {
-    const browser = await chrome.puppeteer.launch(options);
+    const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
 
