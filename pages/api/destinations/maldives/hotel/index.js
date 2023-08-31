6129ce0ca00cb395d3e5ca8ea1969da8930b50d7
0x-m1cro <0xm1cro@proton.me>
2023-08-31 15:31:26 +0500

    updated chromium

--- pages/api/destinations/maldives/hotel/index.js
+++ pages/api/destinations/maldives/hotel/index.js
@@ -1,17 +1,24 @@
-const chrome = require("chrome-aws-lambda");
+//const chrome = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer");
+const chromium = require("@sparticuz/chromium");
 
 export default async function handler(req, res) {
   let query = req.query;
   const { hotelid, checkin, checkout, rooms } = query;
 
-  const options = {
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless,
-  };
+  // const options = {
+  //   args: chrome.args,
+  //   executablePath: await chrome.executablePath,
+  //   headless: chrome.headless,
+  // };
 
   try {
-    const browser = await chrome.puppeteer.launch(options);
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath:
+      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
+      headless: true,
+    });
 
     const page = await browser.newPage();
 
