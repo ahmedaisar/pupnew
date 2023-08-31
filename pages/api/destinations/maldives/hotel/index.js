90441a16276cb4e533353d13d6a428bc024b4542
0x-m1cro <0xm1cro@proton.me>
2023-08-31 17:51:04 +0500

    playwright installed

--- pages/api/destinations/maldives/hotel/index.js
+++ pages/api/destinations/maldives/hotel/index.js
@@ -1,7 +1,6 @@
 //const chrome = require("chrome-aws-lambda");
-const puppeteer = require("puppeteer-core");
-const chromium = require("@sparticuz/chromium");
-
+import * as playwright from 'playwright-aws-lambda'
+ 
 export default async function handler(req, res) {
   let query = req.query;
   const { hotelid, checkin, checkout, rooms } = query;
@@ -13,28 +12,20 @@ export default async function handler(req, res) {
   // };
 
   try {
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath,
-      headless: true,
-    });
-
-    const page = await browser.newPage();
+    const browser = await playwright.launchChromium({ headless: true })
+    const context = await browser.newContext({ screen: { width: 4096, height: 4096 } })
+    const page = await context.newPage()
 
     await page.goto(
-      `https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${rooms}&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
-      {
-        waitUntil: "networkidle2",
-      }
+      `https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${rooms}&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`
     );
     //await page.waitForTimeout(500);
     let html = await page.evaluate(() => {
-      let body = document.querySelector("body").innerText;
+    let body = document.querySelector("body").innerText;
       //let pre = document.querySelector("pre").innerHTML;
       return JSON.parse(body);
     });
-    console.log("Chromium:", await browser.version());
-    console.log("Page Title:", await page.title());
+
     await browser.close();
     res.status(200).json(html);    
     console.log(html);
