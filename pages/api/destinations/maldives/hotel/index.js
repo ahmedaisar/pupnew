4cef2f8bda1f8d2597e984e73accd30eaddb00ea
0x-m1cro <0xm1cro@proton.me>
2023-08-31 17:11:31 +0500

    updated chromium#7

--- pages/api/destinations/maldives/hotel/index.js
+++ pages/api/destinations/maldives/hotel/index.js
@@ -1,6 +1,6 @@
 //const chrome = require("chrome-aws-lambda");
 const puppeteer = require("puppeteer-core");
-const chromium = require("@sparticuz/chromium-min");
+const chromium = require("@sparticuz/chromium");
 
 export default async function handler(req, res) {
   let query = req.query;
@@ -15,10 +15,8 @@ export default async function handler(req, res) {
   try {
     const browser = await puppeteer.launch({
       args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(),
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
+      executablePath: await chromium.executablePath,
+      headless: true,
     });
 
     const page = await browser.newPage();
@@ -26,17 +24,19 @@ export default async function handler(req, res) {
     await page.goto(
       `https://hotelscan.com/combiner/${hotelid}?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${rooms}&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&toas=resort&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
       {
-        waitUntil: "load",
+        waitUntil: "networkidle2",
       }
     );
-    await page.waitForTimeout(500);
+    //await page.waitForTimeout(500);
     let html = await page.evaluate(() => {
       let body = document.querySelector("body").innerText;
-      let pre = document.querySelector("pre").innerHTML;
+      //let pre = document.querySelector("pre").innerHTML;
       return JSON.parse(body);
     });
+    console.log("Chromium:", await browser.version());
+    console.log("Page Title:", await page.title());
     await browser.close();
-    res.status(200).json(html);
+    res.status(200).json(html);    
     console.log(html);
   } catch (error) {
     console.log(error);
