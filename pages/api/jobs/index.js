9b316786a35e537e95a773f9b35d27a4a6370394
0x-m1cro <0xm1cro@proton.me>
2023-08-31 16:45:48 +0500

    updated chromium#3

--- pages/api/jobs/index.js
+++ pages/api/jobs/index.js
@@ -1,55 +1,55 @@
-const puppeteer = require("puppeteer");
-const cheerio = require('cheerio');
-const fs = require('fs');
+// const puppeteer = require("puppeteer");
+// const cheerio = require('cheerio');
+// const fs = require('fs');
 
-export default async function handler(req, res) {
-  // const options = {
-  //   //args: chrome.args,
-  //   executablePath: await puppeteer.executablePath,
-  //  // headless: chrome.headless,
-  // };
+// export default async function handler(req, res) {
+//   // const options = {
+//   //   //args: chrome.args,
+//   //   executablePath: await puppeteer.executablePath,
+//   //  // headless: chrome.headless,
+//   // };
 
-  try {
-    const browser = await puppeteer.launch();
+//   try {
+//     const browser = await puppeteer.launch();
 
-    const page = await browser.newPage();
+//     const page = await browser.newPage();
 
-    await page.goto('https://www.job-maldives.com/', {waitUntil: 'load', timeout: 0});
-    //await page.waitForTimeout(2000);
-    //getting access to the raw HTML
-    const pageData = await page.evaluate(() => {
-        return {
-        html: document.documentElement.innerHTML,
-        };
-    });
-    let data = [];
-    let title;
-    let date;
-    let link;
-    //parsing the HTML and picking our elements
-    const $ = cheerio.load(pageData.html);
-    let alljobs = $('.recent-posts-list');
-        alljobs.each((index, element) => {
-        title = $(element).find('div.recent-post-title a').text();
-        link = $(element).find('div.recent-post-title a').attr('href');
-        date = $(element).find('div.recent-posts-details').text();
-       // newlink = link.replace('https://www.jobmaldives.com/2023/', '');
+//     await page.goto('https://www.job-maldives.com/', {waitUntil: 'load', timeout: 0});
+//     //await page.waitForTimeout(2000);
+//     //getting access to the raw HTML
+//     const pageData = await page.evaluate(() => {
+//         return {
+//         html: document.documentElement.innerHTML,
+//         };
+//     });
+//     let data = [];
+//     let title;
+//     let date;
+//     let link;
+//     //parsing the HTML and picking our elements
+//     const $ = cheerio.load(pageData.html);
+//     let alljobs = $('.recent-posts-list');
+//         alljobs.each((index, element) => {
+//         title = $(element).find('div.recent-post-title a').text();
+//         link = $(element).find('div.recent-post-title a').attr('href');
+//         date = $(element).find('div.recent-posts-details').text();
+//        // newlink = link.replace('https://www.jobmaldives.com/2023/', '');
 
-        data.push({
-            'title': title,
-            'date': date,
-            'link': link
-            })
-      }); 
+//         data.push({
+//             'title': title,
+//             'date': date,
+//             'link': link
+//             })
+//       }); 
 
-    fs.writeFile('./json/jobs.json', data, function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-    res.status(200).json(data);
-    await browser.close();    
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     fs.writeFile('./json/jobs.json', data, function(err) {
+//         if (err) {
+//             console.log(err);
+//         }
+//     });
+//     res.status(200).json(data);
+//     await browser.close();    
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
